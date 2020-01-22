78e338194d1408b2801aa58e60caf465080fa90d
VIVIFY <vivify@MacBook-Pro.local>
2020-01-22 11:14:58 +0100

    #LVNA-15 Add todo controller

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -3,8 +3,109 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use App\Todo;
+
+use JWTAuth;
 
 class TodoController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $user = JWTAuth::parseToken()->authenticate();
+        return Todo::where('user_id', $user->id)
+                    ->orderBy('created_at', 'asc')
+                    ->get();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $user = JWTAuth::parseToken()->authenticate();
+
+        $todo = new Todo;
+        $todo->title = $request->title;
+        $todo->user_id = $user->id;
+        try {
+            $todo->save();
+            return $todo;
+        } catch (Exception $e) {
+            Log::error($e->getMessage());
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = JWTAuth::parseToken()->authenticate();
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $user = JWTAuth::parseToken()->authenticate();
+        $todo = Todo::where('user_id', $user->id)->findOrFail($id);
+        $todo->title = $request->title;
+        $todo->completed = $request->completed;
+        $todo->important = $request->important;
+        try {
+            $todo->save();
+            return $todo;
+        } catch (Exception $e) {
+            Log::error($e->getMessage());
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Todo::findOrFail($id)->delete();
+    }
 }
